b60bbaa3c25c1cdb85077855be4d40d62b377aec
G (Bruce) McLean <mclgb001@mymail.unisa.edu.au>
2026-02-10 15:44:00 +1000

    Protect Supabase keys using env variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'home_page.dart';
@@ -5,6 +6,13 @@ import 'pages/vehicle_log_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
 
   await Supabase.initialize(
     url: 'https://phkwizyrpfzoecugpshb.supabase.co',
